0b038dfc080546381ce78e7684ecb911011901f4
Kyle Butler <kylbutlr@gmail.com>
2019-03-27 17:10:45 -0700

    auto resize

--- src/App.js
+++ src/App.js
@@ -41,7 +41,17 @@ class App extends Component {
   }
 
   componentDidMount() {
+    this.runResize();
     this.getSavedSession();
+  }
+
+  runResize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
   }
 
   getSavedSession() {
