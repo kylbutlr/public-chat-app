02c3397d7bca1881aba46c723ba22268a991b203
Kyle Butler <kylbutlr@gmail.com>
2019-04-01 18:11:07 -0700

    solving heroku error

---
+++ src/Components/LoginForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export default class LoginForm extends Component {
+  render() {
+    return (
+      <div className='login-form'>
+        <form className='form' onSubmit={this.props.onSubmit}>
+          <input
+            type='text'
+            name='username'
+            id='loginUsername'
+            className='input'
+            placeholder='Username'
+            autoComplete='off'
+            required
+            value={this.props.username}
+            onChange={e => this.props.onChange('username', e)}
+          />
+          <br />
+          <input
+            type='password'
+            name='password'
+            id='loginPassword'
+            className='input'
+            placeholder='Password'
+            autoComplete='off'
+            required
+            value={this.props.password}
+            onChange={e => this.props.onChange('password', e)}
+          />
+          <br />
+          <input className='button' type='submit' value='Login' />
+        </form>
+        <div className='or'>or</div>
+        <button className='button' onClick={this.props.onClick}>
+          Register
+        </button>
+      </div>
+    );
+  }
+}
